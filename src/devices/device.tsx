ddd7d9f2ce504292d6f163f48dab98f877404824
Alykam Burdzaki <alykam@gmail.com>
2021-06-28 19:34:13 -0300

    change drag logic to custom hook

--- src/devices/device.tsx
+++ src/devices/device.tsx
@@ -1,61 +1,18 @@
-import { useFrame, useThree } from '@react-three/fiber'
-import { ThreeEvent } from '@react-three/fiber/dist/declarations/src/core/events'
-import React, { useCallback, useContext, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import * as THREE from 'three'
 
-import { camContext } from '../contexts/camera'
+import { useDrag } from '../hooks/useDrag'
 import { useHover } from '../hooks/useHover'
 
-const plane = new THREE.Plane()
-const worldPosition = new THREE.Vector3()
-
 export const Device = () => {
-    const [isDragging, setIsDragging] = useState(false)
-    const { raycaster, camera, mouse } = useThree()
     const [bindHover, hovered] = useHover()
     const ref = useRef<THREE.Mesh>()
-    const [, enableControls] = useContext(camContext)
-    const offset = useRef(new THREE.Vector3())
 
-    const intersects = useRef(new THREE.Vector3())
-
-    const onPointerDown = useCallback(
-        (e: ThreeEvent<PointerEvent>) => {
-            worldPosition.copy(e.object.position)
-
-            plane
-                .setFromNormalAndCoplanarPoint(
-                    camera.getWorldDirection(plane.normal),
-                    worldPosition,
-                )
-                .normalize()
-
-            raycaster.setFromCamera(mouse, camera)
-            raycaster.ray.intersectPlane(plane, intersects.current)
-            offset.current.copy(e.object.position).sub(intersects.current)
-
-            setIsDragging(true)
-            enableControls(false)
-        },
-        [setIsDragging, camera, enableControls, intersects, offset, raycaster, mouse],
-    )
-
-    const onPointerUp = useCallback(() => {
-        setIsDragging(false)
-        enableControls(true)
-    }, [setIsDragging, enableControls])
-
-    useFrame(() => {
-        raycaster.setFromCamera(mouse, camera)
-        if (isDragging && raycaster.ray.intersectPlane(plane, intersects.current)) {
-            const newP = intersects.current.add(offset.current)
-            ref.current?.position.copy(newP)
-        }
-    })
+    const { bindDrag } = useDrag({ ref })
 
     return (
         <group>
-            <mesh {...bindHover} onPointerDown={onPointerDown} onPointerUp={onPointerUp} ref={ref}>
+            <mesh {...bindHover} {...bindDrag} ref={ref}>
                 <boxGeometry args={[1, 1, 1]} />
                 <meshStandardMaterial color={hovered ? 'cyan' : 'white'} />
             </mesh>
--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -1,45 +1,74 @@
-import { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { Camera, useFrame, useThree } from '@react-three/fiber'
+import { ThreeEvent } from '@react-three/fiber/dist/declarations/src/core/events'
+import { MutableRefObject, useCallback, useContext, useRef, useState } from 'react'
+import * as THREE from 'three'
+import { BufferGeometry, Material, Mesh } from 'three'
 
 import { camContext } from '../contexts/camera'
 
-export function useDrag(onDrag: any, onEnd: any) {
-    const [active, setActive] = useState<any>(false)
-    const [, toggle] = useContext(camContext)
-    const activeRef = useRef()
+interface UseDragOptions {
+    ref?: MutableRefObject<Mesh<BufferGeometry, Material | Material[]> | undefined>
+    camera?: Camera
+    raycaster?: THREE.Raycaster
+    mouse?: THREE.Vector2
+}
 
-    const down = useCallback(
-        (e) => {
-            setActive(true)
-            toggle(false)
-            e.stopPropagation()
-            e.target.setPointerCapture(e.pointerId)
+export const useDrag = (options?: UseDragOptions) => {
+    const [isDragging, setIsDragging] = useState(false)
+    const three = useThree()
+    const defaultRef = useRef<THREE.Mesh>()
+
+    const mouse = options?.mouse || three.mouse
+    const raycaster = options?.raycaster || three.raycaster
+    const camera = options?.camera || three.camera
+    const ref = options?.ref || defaultRef
+
+    const [, enableControls] = useContext(camContext)
+
+    const plane = useRef(new THREE.Plane())
+    const worldPosition = useRef(new THREE.Vector3())
+    const offset = useRef(new THREE.Vector3())
+    const intersects = useRef(new THREE.Vector3())
+
+    const onPointerDown = useCallback(
+        (e: ThreeEvent<PointerEvent>) => {
+            worldPosition.current.copy(e.object.position)
+
+            plane.current
+                .setFromNormalAndCoplanarPoint(
+                    camera.getWorldDirection(plane.current.normal),
+                    worldPosition.current,
+                )
+                .normalize()
+
+            raycaster.setFromCamera(mouse, camera)
+            raycaster.ray.intersectPlane(plane.current, intersects.current)
+            offset.current.copy(e.object.position).sub(intersects.current)
+
+            setIsDragging(true)
+            enableControls(false)
         },
-        [toggle],
+        [setIsDragging, camera, enableControls, intersects, offset, raycaster, mouse],
     )
-    const up = useCallback(
-        (e) => {
-            setActive(false)
-            toggle(true)
-            e.target.releasePointerCapture(e.pointerId)
-            if (onEnd) {
-                onEnd()
-            }
-        },
-        [onEnd, toggle],
-    )
-    const move = useCallback(
-        (event) => {
-            if (activeRef.current) {
-                event.stopPropagation()
-                console.log(event, onDrag)
-            }
-            // activeRef.current && (, onDrag(.unprojectedPoint))
-        },
-        [onDrag],
-    )
-    useEffect(() => {
-        activeRef.current = active
+
+    const onPointerUp = useCallback(() => {
+        setIsDragging(false)
+        enableControls(true)
+    }, [setIsDragging, enableControls])
+
+    useFrame(() => {
+        raycaster.setFromCamera(mouse, camera)
+        if (isDragging && raycaster.ray.intersectPlane(plane.current, intersects.current)) {
+            const newP = intersects.current.add(offset.current)
+            ref.current?.position.copy(newP)
+        }
     })
 
-    return { onPointerDown: down, onPointerUp: up, onPointerMove: move }
+    return {
+        ref,
+        bindDrag: {
+            onPointerUp,
+            onPointerDown,
+        },
+    }
 }
